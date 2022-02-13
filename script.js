466d430502dce53a678708e4207ef1aa967c26b2
terjeee <96795863+terjeee@users.noreply.github.com>
2022-02-13 19:47:50 +0100

    fjernet kommentarer

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 'use strict';
 
-/*
-console.log(document.querySelector(`.message`).textContent);
-
-document.querySelector(`.message`).textContent = `Correct Number!`;
-console.log(document.querySelector(`.message`).textContent);
-
-document.querySelector(`.number`).textContent = 13;
-document.querySelector(`.score`).textContent = 10;
-
-document.querySelector(`.guess`).value = 23;
-console.log(document.querySelector(`.guess`).value);
-*/
-
-// eventListener ()
-
-// generate secretNumber
-let secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20) + 1; // generate secretNumber
 let scoreCounter = 20;
 let highScore = 0;
 
